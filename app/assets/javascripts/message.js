a761b927aa79ceed2b29f072f95513ae928d9fc3
yuto1234 <mjgda85200@yahoo.co.jp>
2019-07-24 17:31:38 +0900

    add-done-method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class="message">
+                <div class="message__info">
+                <div class="message__info--user-name">
+                ${ message.name }
+                </div>
+                <div class="message__info--post-time">
+                ${ message.created_at }
+                </div>
+                </div>
+                <div class="message__post">`
+                if (message.content != ""){
+                  html += `<p class="message__post">${ message.content }</p>`
+                }
+                if (message.image != null){
+                  html += `<img src='${ message.image }', class='lower-message__image'>`
+                }
+
+                html += '</div>'
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +33,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-space__main').append(html)
+      $('.input-form__box').val('')
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
-json.message @message.comment
+json.content @message.content
 json.image @message.image.url
-json.user_id @message.user_id
+json.name @message.user.name
 json.created_at @message.created_at

a283942be43fe01abd06f900338242b18ef7823c
yuto1234 <mjgda85200@yahoo.co.jp>
2019-07-25 13:24:03 +0900

    Add-ajax

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,5 +1,27 @@
 $(function(){
   $(".chat-group-form__input").on("keyup", function(){
-    console.log("ok");
+    var input = $(this).val();
+    
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(dataes){
+        $("#user-search-result").empty();
+        if (dataes.length !== 0){
+          dataes.forEach(function(data){
+            appendProduct(data);
+          });
+        }
+        else {
+          appendErrMsgToHTML("一致するユーザーはいません");
+        }
+      })
+    .fail(function(){
+      alert('error');
+    });
   });
 });

e0f0f94d07fba5c3bb27c9d85091dc210fc45482
yuto1234 <mjgda85200@yahoo.co.jp>
2019-07-25 16:04:30 +0900

    Add-HTML

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,4 +1,23 @@
 $(function(){
+  var search_result = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+                </div>`
+
+    search_result.append(html);
+  }
+
+  function appendNoUser(fail) {
+    var html = `<p>
+                  <div class="chat-group-user__name">${ fail }</p>
+                </p>`
+
+    search_result.append(html);
+  }
+
   $(".chat-group-form__input").on("keyup", function(){
     var input = $(this).val();
     
@@ -8,20 +27,19 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
-
-    .done(function(dataes){
-        $("#user-search-result").empty();
-        if (dataes.length !== 0){
-          dataes.forEach(function(data){
-            appendProduct(data);
+    .done(function(user){
+      $("#user-search-result").empty();
+        if (user.length !== 0){
+          user.forEach(function(user){
+            appendUser(user);
           });
         }
         else {
-          appendErrMsgToHTML("一致するユーザーはいません");
+          appendNoUser("一致するユーザーはいません");
         }
       })
     .fail(function(){
-      alert('error');
-    });
+      alert('ユーザー検索に失敗しました');
+    })
   });
 });
